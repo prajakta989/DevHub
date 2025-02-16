7749f2fd28db29c2b99b5b927ecaec41007c431a
prajakta989 <prajaktanaik98@gmail.com>
2025-02-16 11:59:03 +0530

    exploring GET,POST DELETE http requests

--- src/app.js
+++ src/app.js
@@ -2,12 +2,28 @@ const express = require('express')
 
 const app = express();
 
-
-app.use((req,res) =>{
-    res.send("hello from server")
+ 
+app.use("/user", (req,res) => {
+    res.send("Hahahahahhaha")
 })
+app.get("/user", (req,res) => {
+    res.send({firstname:"Prajakta", lastname:"Naik Mule"})
+})
+
+app.post("/user", (req,res)=> {
+    res.send("Registered user successfully!")
+})
+app.delete("/user", (req,res) => {
+    res.send("Deleted user successfully!")
+})
+
+
 app.use("/test",(req,res) =>{
     res.send("hello from test")
+})
+
+app.use("/",(req,res) =>{          //kind of wild card route
+    res.send("hello from server")
 })
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
