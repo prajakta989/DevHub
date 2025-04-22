c13d47d14473a59293675f8519569c8ad7327406
Prajakta Naik <prajaktanaik98@gmail.com>
2025-04-22 11:15:03 +0530

    added few validations to user schema

--- src/models/User.js
+++ src/models/User.js
@@ -4,26 +4,52 @@ const {Schema} = mongoose
 const userSchema = new Schema({
     firstName: {
         type: String,
-        required: true
+        required: true,
+        minLength: 3, 
+        maxLength: 255, 
     },
     lastName:{
         type:String
     },
     emailId:{
         type:String, 
-        required:true
+        required:true,
+        unique : true, 
+        lowercase: true, 
+        trim: true, 
     },
     password:{
         type:String,
         required: true
     },
     age:{
-        type:Number
+        type:Number,
+        min:18,
+        max: 50
     },
     gender:{
         type:String,
-    }
-
+    },
+    photoUrl:{
+        type: String
+    },
+    about:{
+        type: String,
+        default: "This is default about information about the user"
+    },
+    skills:{
+        type: [String]
+    },
+    // location:{
+    //     type: String,
+    //     minLength: 20,
+    //     maxLength: 255
+    // },
+    // phoneNumber:{
+    //     type: String,
+    //     minLength: 10,
+    //     maxLength: 15
+    // }
 })
 
 const User = mongoose.model("User", userSchema)
