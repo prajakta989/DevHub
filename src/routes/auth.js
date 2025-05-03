f9dd12ad43d1ddf2a9fa6e3f9be0a14e875fcf9d
prajakta989 <prajaktanaik98@gmail.com>
2025-05-03 13:48:28 +0530

    logout API

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -62,4 +62,9 @@ authRouter.post("/login", async (req, res) => {
   }
 });
 
+authRouter.post("/logout", (req,res) => {
+    res.cookie("token",null, {expires: new Date(Date.now())})
+    res.send("Logout Successfully!!")
+})
+
 module.exports = authRouter;
